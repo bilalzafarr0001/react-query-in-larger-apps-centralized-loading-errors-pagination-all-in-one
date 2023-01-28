aa3fc0f0254bac4502b417c1fa87ca7a648cbd5d
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2023-01-28 16:41:37 +0500

    first commit

---
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+import { toast } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
+
+import type { Treatment } from '../../../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+
+// for when we need a query function for useQuery
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
+
+export function useTreatments(): Treatment[] {
+  // TODO: get data from server via useQuery
+
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  return data;
+}
