1e64401303a180b8f7725a8ebe8aa13f9a573387
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2023-01-29 14:08:18 +0500

    Add filtered pselectFn function in Staff component to filter data based on radio button selection

--- client/src/components/staff/hooks/useStaff.ts
+++ client/src/components/staff/hooks/useStaff.ts
@@ -1,14 +1,14 @@
-import { Dispatch, SetStateAction, useState } from 'react';
-import { useQuery, useQueryClient } from 'react-query';
+import { Dispatch, SetStateAction, useCallback, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
-import type { Staff } from '../../../../../shared/types';
-import { axiosInstance } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
-import { filterByTreatment } from '../utils';
+import type { Staff } from "../../../../../shared/types";
+import { axiosInstance } from "../../../axiosInstance";
+import { queryKeys } from "../../../react-query/constants";
+import { filterByTreatment } from "../utils";
 
 // for when we need a query function for useQuery
 async function getStaff(): Promise<Staff[]> {
-  const { data } = await axiosInstance.get('/staff');
+  const { data } = await axiosInstance.get("/staff");
   return data;
 }
 
@@ -20,11 +20,18 @@ interface UseStaff {
 
 export function useStaff(): UseStaff {
   // for filtering staff by treatment
-  const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState("all");
+
+  const selectFn = useCallback(
+    (unfilteredStaff) => filterByTreatment(unfilteredStaff, filter),
+    [filter]
+  );
 
   // TODO: get data from server via useQuery
   const fallback = [];
-  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff, {
+    select: filter !== "all" ? selectFn : undefined,
+  });
 
   return { staff, filter, setFilter };
 }
