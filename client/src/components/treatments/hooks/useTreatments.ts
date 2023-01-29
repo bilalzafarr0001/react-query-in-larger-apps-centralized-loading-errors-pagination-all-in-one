7289d0f00c8cbb1bd1e286a9d7cd4ccfbcee04f0
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2023-01-29 16:03:40 +0500

    Add staleTime and cacheTime in useTreatment hook and pass options in useQuery hook

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -16,11 +16,20 @@ export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
 
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 minutes
+    cacheTime: 900000, // 15 minutes
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
   return data;
 }
 
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 minutes
+    cacheTime: 900000, // 15 minutes
+  });
 }
