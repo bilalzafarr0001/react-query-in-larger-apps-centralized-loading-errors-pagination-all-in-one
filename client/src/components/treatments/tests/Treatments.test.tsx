f51dfbb946730d92301b25ba79d01a26277b376e
Muhammad Bilal <muhammadbilal@nxb.com.pk>
2023-01-31 20:47:40 +0500

    Add test case for treatment in Treatment.tsx.test file

--- client/src/components/treatments/tests/Treatments.test.tsx
+++ client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-
+import { screen } from '@testing-library/react';
+import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
-  // write test here
-  render(<Treatments />);
+test('renders response from query', async () => {
+  renderWithQueryClient(<Treatments />);
+  const treatmentTitles = await screen.findByRole('Heading', {
+    name: 'Massage',
+  });
+  expect(treatmentTitles).toHaveLength(1);
 });
--- client/src/test-utils/index.tsx
+++ client/src/test-utils/index.tsx
@@ -1,11 +1,27 @@
-import { render } from '@testing-library/react';
-
+import { RenderResult, render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactElement } from 'react';
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
 
-// from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = (): React.FC => {
-//   const queryClient = generateQueryClient();
-//   return ({ children }) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+const generateQueryClient = () => {
+  return new QueryClient();
+};
+
+export function renderWithQueryClient(
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult {
+  const queryClient = client ?? generateQueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+}
+
+//from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
+
+export const createQueryClientWrapper = (): React.FC => {
+  const queryClient = generateQueryClient();
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
